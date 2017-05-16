1da93788e697f852751b5949a19d94f21179470b
Blueibis <jennie.li.2011@gmail.com>
2017-05-16 14:31:30 -0400

    exercise 0; arrays and hashes

---
+++ exercise.rb
@@ -0,0 +1,14 @@
+fav_colours = ["blue", "white", "purple", "grey", "pink"]
+ages = [22, 16, 51, 55]
+coin_flip = [false, true, false, true, false]
+fav_artists = ["Maroon 5", "Christina Grimmie", "Ashley Barret"]
+fav_colours_sym = [:blue, :white, :purple, :grey, :pink]
+
+definition = {
+  "Lorem" => "Placeholder for meaningful content",
+  "Unattire" => "To divest of attire; to undress",
+  "Lapidific" => "Forming or converting into stone"
+}
+fav_movies = {"Mass Appeal" => 1984, "Money Madness" => 1948, "The Incredibles" => 2004}
+some_cities = {"Bangkok" => 14565547, "Nairobi" => 3375000, "Tehran" => 8200000}
+f_and_f = {"Justin" => 22, "Jack" => 16, "Bettie" => 51, "Frank" => 55}
